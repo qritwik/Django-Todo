7a2798ccdba5f41b49015c04b1c52607f18c8959
Ritwik <qritwik@gmail.com>
2019-03-11 20:09:19 +0530

    date time

--- todo/settings.py
+++ todo/settings.py
@@ -185,3 +185,6 @@ LOGIN_URL = '/user_login'
 django_heroku.settings(locals())
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S'
+USE_L10N = False
